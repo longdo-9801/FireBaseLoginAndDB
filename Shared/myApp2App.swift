e7007a269d4400505cdc10c3ac960a939ae309c5
Long Do <2ndbenben98@gmail.com>
2022-09-12 15:45:46 +0700

    Initial Commit

---
+++ Shared/myApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  myApp2App.swift
+//  Shared
+//
+//  Created by Long, Do Ha Minh on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct myApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
