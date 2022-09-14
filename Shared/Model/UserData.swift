8b33d4ec8772d5d72ad9b5f2ca26b15bb10d82d4
Long Do <2ndbenben98@gmail.com>
2022-09-14 09:55:15 +0700

    userData upload

---
+++ Shared/Model/UserData.swift
@@ -0,0 +1,14 @@
+//
+//  UserData.swift
+//  myApp2 (iOS)
+//
+//  Created by Long, Do Ha Minh on 14/09/2022.
+//
+
+import Foundation
+
+struct UserData {
+    var name : String
+    var email : String
+    var icon : String = "A6MProfile"
+}
